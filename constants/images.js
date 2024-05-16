0fcf9a6ab1f2f210455c9225b59046b419ce784a
hafeex <you@example.com>
2024-05-16 18:01:23 +0530

    last commit

--- constants/images.js
+++ constants/images.js
@@ -1,7 +1,11 @@
-import img1 from '../assets/portfoli/kone.jpg'
+import img3 from '../assets/portfoli/62.jpg'
+import img2 from '../assets/portfoli/img32.webp'
+import img1 from '../assets/portfoli/image.jpg'
+import img4 from '../assets/portfoli/market.jpg'
+
 
 export const portImages = [
-    img1,img1,img1,img1,img1,img1,img1,img1,img1,img1,img1,img1
+    img1,img4,img3,img2,img1,img3,img4,img2,img3,img2,img4,img3
 
 ]
 
